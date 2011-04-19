6032a0707619c4321374e91be43c5be30f10e910
luo tao <lotreal@gmail.com>
2011-04-19 11:16:32 +0800

    xxoo

--- src/after_install.sh
+++ src/after_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source h.sh
 
+# echo "0 3 1 * * /sbin/reboot" >> /var/spool/cron/root
+# echo "0 * * * * ntpdate us.pool.ntp.org" >> /var/spool/cron/root
+
 xlog notice "【安装暂停】现在将进行 mysql 设置，请输入密码继续……"
 echo "设置 root 密码："
 echo "随机密码：$(xmkpasswd) $(xmkpasswd) $(xmkpasswd) $(xmkpasswd) $(xmkpasswd)"
--- src/h.sh
+++ src/h.sh
@@ -1,47 +1,51 @@
 if [[ $_included != "t" ]]; then
-    __PrintDebug=1
+  __PrintDebug=1
 
-    wd=$(dirname $(readlink -f $0))
-    swd=$(cd $wd/../ && pwd)
+  # 调用 h.sh 的脚本(比如 install.sh)所在目录。
+  wd=$(dirname $(readlink -f $0))
+  # 工程根目录
+  swd=$(cd $wd/../ && pwd)
+  # 安装脚本目录
+  script_dir=$swd/src
+  # 安装文件下载缓存，如没有则自动下载
+  cache_dir=$swd/archives
+  mkdir -p $cache_dir
 
-    script_dir=$swd/src
-    cache_dir=$swd/archives
-    mkdir -p $cache_dir
+  install_log=$swd/log/install.log
+  build_dir=$swd/build
 
-    install_log=$swd/log/install.log
-    build_dir=$swd/build
-    autosave_dir=/webserver/autosave
+  default_profile=$script_dir/etc
+  source $default_profile/config.ini
 
-    # source $script_dir/mlog.sh
-    source $script_dir/functions.sh
+  source $script_dir/functions.sh
 
-    default_profile=$script_dir/etc
-    user_profile=$default_profile
-    while getopts ":C:" Option
-    do
-        case $Option in
-            C     ) user_profile=$script_dir/profile/$OPTARG; echo "use profile $user_profile";;
-            *     ) echo "Unimplemented option chosen.";;   # 默认情况的处理
-        esac
-    done
-    shift $(($OPTIND - 1))
-    source $default_profile/config.ini
-    if [[ "$default_profile" != "$user_profile" ]]; then
-        source $user_profile/config.ini
-    fi
+  xlog debug "##############################################################################"
+  xlog debug "#     启动 autosrv ..."
+  xlog debug "# 配置文件: $default_profile/config.ini"
+  xlog debug "#   函数库: $script_dir/functions.sh"
 
-    xlog debug "##############################################################################"
-    xlog debug "启动 autosrv ..."
-    xlog debug "配置文件: $script_dir/config.sh"
-    xlog debug "函数库  : $script_dir/functions.sh"
+  while getopts ":C:" Option
+  do
+    case $Option in
+      C )
+        user_profile=$script_dir/profile/$OPTARG
+        if [[ "$default_profile" != "$user_profile" ]]; then
+          xlog debug "# 用户配置: $user_profile/config.ini"
+          source $user_profile/config.ini
+        fi
+        ;;
+      * ) echo "Unimplemented option chosen.";;   # 默认情况的处理
+    esac
+  done
+  shift $(($OPTIND - 1))
 
-    if [[ ${__LogEnabled:-1} -eq 1 ]]; then
-	xlog debug "运行日志：$install_log"
-	xautosave $install_log
-	mkdir -p $(dirname $install_log)
-	exec > >(tee $install_log) 2>&1
-    fi
-    xlog debug "##############################################################################"
+  if [[ ${__LogEnabled:-1} -eq 1 ]]; then
+	  xlog debug "# 运行日志：$install_log"
+	  xautosave $install_log
+	  mkdir -p $(dirname $install_log)
+	  exec > >(tee $install_log) 2>&1
+  fi
+  xlog debug "##############################################################################"
 
-    _included=t
+  _included=t
 fi
