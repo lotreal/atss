6a0443b656422b75acbea2a556a5897e7b51a550
luo tao <lotreal@gmail.com>
2011-05-20 11:14:39 +0800

    add download_all

--- src/install_all.sh
+++ src/install_all.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 . h.sh
 
+download_all() {
+  for i in \
+    "$bitlbee" \
+    "$php_libiconv" \
+    "$php_libmhash" \
+    "$php_libmcrypt" \
+    "$mcrypt" \
+    "$cmake" \
+    "$mysql" \
+    "$php" \
+    "$php_fpm" \
+    "$php5_3" \
+    "$eaccelerator" \
+    "$imagemagick" \
+    "$pecl_memcache" \
+    "$pecl_pdo_mysql" \
+    "$pecl_imagick" \
+    "$zend_optimizer" \
+    "$pcre" \
+    "$nginx" \
+    "$phpmyadmin"
+  do
+    if [ -s $cache_dir/$i ]; then
+      echo "$i [found]"
+    else
+      echo "Error: $i not found!!!download now......"
+      echo wget $i -P $cache_dir/
+    fi
+  done
+}
+download_all
+
 xinstall bitlbee
 xinstall php_libiconv
 xinstall php_libmhash
